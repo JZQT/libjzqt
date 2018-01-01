ef988e4c90b8f5b4833474100edc95ca2e1c7075
JZQT <jzqt@witcoder.com>
2018-01-01 18:02:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,26 @@
 
 from setuptools import setup
 
+import jzqt
+
 
 setup(
     name='jzqt',
-    version='0.1.0',
+    version=jzqt.__version__,
     packages=['jzqt'],
     author='JZQT',
     author_email='561484726@qq.com',
     url="https://github.com/JZQT/jzqt",
     description="Python tools library",
     license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ]
 )
